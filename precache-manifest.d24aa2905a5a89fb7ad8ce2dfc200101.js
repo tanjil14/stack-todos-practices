56eea31a2c0048077956f95b9e330a09a5f04cb0
tanjil <tanjil14@cse.pstu.ac.bd>
2020-10-19 10:08:20 +0600

    Updates

---
+++ precache-manifest.d24aa2905a5a89fb7ad8ce2dfc200101.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "224fb1cff21388132def3397ff6db338",
+    "url": "/stack-todos-practices/index.html"
+  },
+  {
+    "revision": "53985967c2c01c766b1f",
+    "url": "/stack-todos-practices/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "89aa3dcd79eec6a7241e",
+    "url": "/stack-todos-practices/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "53985967c2c01c766b1f",
+    "url": "/stack-todos-practices/static/js/2.8467746f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stack-todos-practices/static/js/2.8467746f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89aa3dcd79eec6a7241e",
+    "url": "/stack-todos-practices/static/js/main.2825c575.chunk.js"
+  },
+  {
+    "revision": "653bb5e3fd13a9ffaa40",
+    "url": "/stack-todos-practices/static/js/runtime-main.c91ee49c.js"
+  }
+]);
